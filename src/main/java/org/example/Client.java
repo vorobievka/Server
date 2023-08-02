aef75491232e1be8ca25c737f841af02160dda9b
VorobievKA <vorobievka@gmail.com>
2023-08-02 21:41:48 +0300

    Init

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        String host = "localhost";
+        int port = 8081;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new
+                     PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new
+                     InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Alex");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
